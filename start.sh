928d70d04e6a38fed4cf82c8135c8c178499251b
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2025-03-12 14:24:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/lk3140318/Autofilter- /Autofilter-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Autofilter-
 fi
-cd /PiroAutoFilterBot
+cd /Autofilter-
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting Autofilter-...."
 python3 bot.py
